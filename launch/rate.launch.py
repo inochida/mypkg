ba0a1647b8e8af269e3afeed5f73e1e372a657d9
inochi <souchin.i.19@gmail.com>
2025-12-27 16:13:25 +0900

    追加

---
+++ launch/rate.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mypkg',
+            executable='talker',
+            name='talker_node',
+            parameters=[{'currencies': ['USD', 'EUR', 'GBP']}]
+        ),
+        Node(
+            package='mypkg',
+            executable='listener',
+            name='listener_node'
+        ),
+    ])
